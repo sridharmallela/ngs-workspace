d62aa92e313e6c9bb0dfcfe1ec8d8801fe9e2871
Sridhar Mallela <sridhar4coding@gmail.com>
2024-12-20 21:40:13 -0600

    chore(repo): updated dependencies for eslint, jest. updated lint staged to include project specific commands

--- .commitlintrc.ts
+++ .commitlintrc.ts
@@ -1,12 +1,14 @@
 import { utils } from '@commitlint/config-nx-scopes';
+import { RuleConfigSeverity } from '@commitlint/types';
 
 export default {
   extends: ['@commitlint/config-conventional', '@commitlint/config-nx-scopes'],
   rules: {
     'scope-enum': async ctx => [
-      2,
+      RuleConfigSeverity.Error,
       'always',
       ['repo', ...(await utils.getProjects(ctx))]
-    ]
+    ],
+    'header-max-length': [RuleConfigSeverity.Error, 'always', 200]
   }
 };
