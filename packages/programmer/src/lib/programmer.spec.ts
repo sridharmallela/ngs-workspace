f558bea75032085caa347d2be41ab783599ef825
Sridhar Mallela <sridhar4coding@gmail.com>
2024-12-20 21:53:38 -0600

    feat(programmer): added initial scaffold project

---
+++ packages/programmer/src/lib/programmer.spec.ts
@@ -0,0 +1,7 @@
+import { programmer } from './programmer';
+
+describe('programmer', () => {
+  it('should work', () => {
+    expect(programmer()).toEqual('programmer');
+  });
+});
---
+++ packages/programmer/src/lib/programmer.ts
@@ -0,0 +1,3 @@
+export function programmer(): string {
+  return 'programmer';
+}
