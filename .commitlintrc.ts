c8347c7f28bebda10c6cc59c2488b2d526c23a8e
Sridhar Mallela <sridhar4coding@gmail.com>
2024-12-22 14:47:39 -0600

    chore(repo): updated commitlint to use local config

--- .commitlintrc.ts
+++ .commitlintrc.ts
@@ -2,13 +2,12 @@ import { utils } from '@commitlint/config-nx-scopes';
 import { RuleConfigSeverity } from '@commitlint/types';
 
 export default {
-  extends: ['@commitlint/config-conventional', '@commitlint/config-nx-scopes'],
+  extends: ['smalella', '@commitlint/config-nx-scopes'],
   rules: {
     'scope-enum': async ctx => [
       RuleConfigSeverity.Error,
       'always',
       ['repo', ...(await utils.getProjects(ctx))]
-    ],
-    'header-max-length': [RuleConfigSeverity.Error, 'always', 200]
+    ]
   }
 };
