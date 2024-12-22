52d5a8b81d99896a9120bafb840ef643c0740a1a
Sridhar Mallela <sridhar4coding@gmail.com>
2024-12-22 15:23:47 -0600

    feat(prettier): added prettier configs initial

--- packages/configs/prettier/src/lib/prettier.ts
+++ packages/configs/prettier/src/lib/prettier.ts
@@ -1,3 +1,57 @@
-export function prettier(): string {
-  return 'prettier';
-}
+import { Config } from 'prettier';
+
+const prettierConfig: Config = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  semi: true,
+  singleAttributePerLine: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  useTabs: false,
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.js', '*.jsx', '*.mjs', '*.cjs'],
+      options: {
+        jsxSingleQuote: true
+      }
+    },
+    {
+      files: ['*.html', '*.yml'],
+      options: {
+        singleQuote: false
+      }
+    },
+    {
+      files: ['*.md'],
+      options: {
+        proseWrap: 'never'
+      }
+    },
+    {
+      files: ['.prettierrc', '*.code-snippets'],
+      options: {
+        parser: 'json'
+      }
+    },
+    {
+      files: ['*.xml'],
+      options: {
+        parser: 'xml',
+        plugins: ['@prettier/plugin-xml'],
+        xmlWhitespaceSensitivity: 'preserve'
+      }
+    }
+  ]
+};
+
+export { prettierConfig };
