bf67b5c83f26d6b69eefb5a947989088f56cd833
Sridhar Mallela <sridhar4coding@gmail.com>
2024-12-26 04:19:35 -0600

    chore(repo): updated tag formation for matching by semver

--- .github/actions/git-tags/index.mjs
+++ .github/actions/git-tags/index.mjs
@@ -10,19 +10,11 @@ try {
   const dateTag = new Date().toISOString().split('T')[0].replaceAll('-', '.');
   debug(`dateTag:${dateTag}`);
   let next = 0;
-  const tags = currentTag.split('.');
-  const dateTags = dateTag.split('.');
-  // check if previous tag is 4 characters, and year, month and date matches
-  if (
-    tags.length > 3 &&
-    !isNaN(Number(tags[3])) &&
-    tags[0] === dateTags[0] &&
-    tags[1] === dateTags[1] &&
-    tags[2] === dateTags[2]
-  ) {
-    next = Number(tags[3]) + 1;
+  const tags = currentTag.split('-');
+  if (tags.length > 1 && tags[0] === dateTag && !isNaN(Number(tags[1]))) {
+    next = Number(tags[1]) + 1;
   }
-  const nextTag = `${dateTag}.${next}`;
+  const nextTag = `${dateTag}-${next}`;
   debug(`nextTag:${nextTag}`);
   info(`currentTag:${currentTag} && nextTag:${nextTag}`);
   setOutput('currentTag', currentTag);
