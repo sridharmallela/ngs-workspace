ad724cb0d888b101612e3706fbca438f543b69a4
Sridhar Mallela <sridhar4coding@gmail.com>
2025-01-24 06:30:43 -0600

    feat(eslint-config): updated to remove no-extra-semi and naming-convention to be matched

--- packages/eslint-config/src/index.ts
+++ packages/eslint-config/src/index.ts
@@ -3,3 +3,5 @@ export * from './lib/rules/formatters';
 export * from './lib/rules/imports';
 export * from './lib/rules/jest';
 export * from './lib/rules/matchers';
+export * from './lib/rules/standards';
+export * from './lib/rules/typescript';
