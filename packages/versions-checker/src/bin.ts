89500eb3c51530a295c2289c77e134cc8f0b40e5
Sridhar Mallela <sridhar4coding@gmail.com>
2025-02-01 20:30:30 -0600

    feat(versions-checker): added bin.ts to provide executable accordingly

---
+++ packages/versions-checker/src/bin.ts
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import { VersionsCheckerCli } from './lib/cli';
+
+export default new VersionsCheckerCli();
--- packages/versions-checker/src/index.ts
+++ packages/versions-checker/src/index.ts
@@ -1,5 +1,2 @@
-#!/usr/bin/env node
-
-import { VersionsCheckerCli } from './lib/cli';
-
-export default new VersionsCheckerCli();
+export * from './lib/cli';
+export * from './lib/checker';
